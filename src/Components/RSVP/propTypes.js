9a876896a980800dcd7b81169b29a29f5e364a39
Abril Herrada <abrilherrada@gmail.com>
2025-03-24 16:34:58 -0300

    Modified guest propTypes to allow companion null

--- src/Components/RSVP/propTypes.js
+++ src/Components/RSVP/propTypes.js
@@ -5,6 +5,11 @@ export const GuestShape = PropTypes.shape({
   attending: PropTypes.bool,
 });
 
+export const CompanionShape = PropTypes.shape({
+  name: PropTypes.string,
+  attending: PropTypes.bool,
+});
+
 export const ChildShape = PropTypes.shape({
   name: PropTypes.string.isRequired,
   attending: PropTypes.bool,
@@ -14,7 +19,10 @@ export const GuestInfoShape = PropTypes.shape({
   _id: PropTypes.string.isRequired,
   mainGuest: GuestShape.isRequired,
   hasCompanion: PropTypes.bool.isRequired,
-  companion: GuestShape,
+  companion: PropTypes.oneOfType([
+    CompanionShape,
+    PropTypes.oneOf([null])
+  ]),
   hasChildren: PropTypes.bool.isRequired,
   children: PropTypes.arrayOf(ChildShape),
   dietaryRestrictionsInGroup: PropTypes.string,
